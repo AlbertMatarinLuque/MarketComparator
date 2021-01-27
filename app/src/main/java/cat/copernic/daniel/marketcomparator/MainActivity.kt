a25d1ab7ca3884829bbdcd53178f5801953d3bed
Albert <matarin.luque.albert@alumnat.copernic.cat>
2021-01-27 20:19:46 +0100

    Habilitació Registre amb Google

--- app/src/main/java/cat/copernic/daniel/marketcomparator/MainActivity.kt
+++ app/src/main/java/cat/copernic/daniel/marketcomparator/MainActivity.kt
@@ -1,26 +1,22 @@
 package cat.copernic.daniel.marketcomparator
 
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.databinding.DataBindingUtil
 import cat.copernic.daniel.marketcomparator.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -38,10 +34,9 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-
-        mAuth = FirebaseAuth.getInstance()
-        //currentUser = mAuth.currentUser!!
-
+       // mAuth = FirebaseAuth.getInstance()
+       // currentUser = mAuth.currentUser!!
+        val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
@@ -54,11 +49,11 @@ class MainActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.authActivity), drawerLayout)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.authActivity), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-        //updateNav(currentUser)
+        setnavView(navView)
+      //  updateNav(currentUser)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -73,21 +68,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun updateNav(currentUser: FirebaseUser){
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val headerView: View
-        headerView = navView.getHeaderView(0)
-        val headerUserName: TextView
-        val headerUserMail: TextView
-       // val headerUserPhoto: ImageView
-        headerUserName = headerView.findViewById(R.id.nav_username)
-        headerUserMail = headerView.findViewById(R.id.nav_user_mail)
-       // headerUserPhoto = headerView.findViewById(R.id.nav_user_photo)
 
-        headerUserMail.setText(currentUser.email)
-        headerUserName.setText(currentUser.displayName)
-      //  headerUserPhoto
-    }
 
 
 }
---
+++ app/src/main/java/cat/copernic/daniel/marketcomparator/Utils.kt
@@ -0,0 +1,46 @@
+package cat.copernic.daniel.marketcomparator
+
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseUser
+
+
+private lateinit var navView:NavigationView
+
+fun setnavView(nav: NavigationView){
+    navView = nav
+}
+
+fun updateNav(currentUser: FirebaseUser){
+    val headerView: View
+    headerView = navView.getHeaderView(0)
+    val headerUserName: TextView
+    val headerUserMail: TextView
+    // val headerUserPhoto: ImageView
+    headerUserName = headerView.findViewById(R.id.nav_username)
+    headerUserMail = headerView.findViewById(R.id.nav_user_mail)
+    // headerUserPhoto = headerView.findViewById(R.id.nav_user_photo)
+
+    headerUserMail.setText(currentUser.email)
+    headerUserName.setText(currentUser.displayName)
+    //  headerUserPhoto
+}
+
+fun updateNavAnonimo(){
+    val headerView: View
+    headerView = navView.getHeaderView(0)
+    val headerUserName: TextView
+    val headerUserMail: TextView
+    // val headerUserPhoto: ImageView
+    headerUserName = headerView.findViewById(R.id.nav_username)
+    headerUserMail = headerView.findViewById(R.id.nav_user_mail)
+    // headerUserPhoto = headerView.findViewById(R.id.nav_user_photo)
+
+    headerUserMail.setText("CUENTA ANONIMA")
+    headerUserName.setText("")
+    //  headerUserPhoto
+}
+
+
