2815b270812aa6d8d2c457d3bed2a9c52f91485b
MSI-DANIEL <leon.herrero.daniel@alumnat.copernic.cat>
2021-01-28 10:48:31 +0100

    Insercion fragments

--- app/src/main/java/cat/copernic/daniel/marketcomparator/MainActivity.kt
+++ app/src/main/java/cat/copernic/daniel/marketcomparator/MainActivity.kt
@@ -2,7 +2,9 @@ package cat.copernic.daniel.marketcomparator
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
@@ -30,7 +32,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
@@ -38,11 +39,12 @@ class MainActivity : AppCompatActivity() {
        // currentUser = mAuth.currentUser!!
         val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+       /* val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
-        }
+        }*/
+        title = "MarketComparator"
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -56,11 +58,22 @@ class MainActivity : AppCompatActivity() {
       //  updateNav(currentUser)
     }
 
-  /*  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
+      menuInflater.inflate(R.menu.main, menu)
         return true
-    }*/
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.trolley) {
+            Toast.makeText(this, "Lista de la compra", Toast.LENGTH_LONG).show()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
