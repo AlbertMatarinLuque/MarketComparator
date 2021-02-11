7a32d08b22958b5f5f47a8f0612cb30a1035a5e9
MSI-DANIEL <leon.herrero.daniel@alumnat.copernic.cat>
2021-02-11 10:17:37 +0100

    Commit de prueba para la explicacion de la memoria sobre las ramas

--- app/src/main/java/cat/copernic/daniel/marketcomparator/AuthActivity.kt
+++ app/src/main/java/cat/copernic/daniel/marketcomparator/AuthActivity.kt
@@ -23,10 +23,7 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 
 class AuthActivity : Fragment() {
