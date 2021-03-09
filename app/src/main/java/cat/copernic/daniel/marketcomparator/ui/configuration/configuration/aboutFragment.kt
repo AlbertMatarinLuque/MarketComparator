3ff9d739df39c3bb8dae230badfeca2383f7e273
Albert <matarin.luque.albert@alumnat.copernic.cat>
2021-03-09 23:38:28 +0100

    - Funcionalidad VideoView

--- app/src/main/java/cat/copernic/daniel/marketcomparator/ui/configuration/configuration/aboutFragment.kt
+++ app/src/main/java/cat/copernic/daniel/marketcomparator/ui/configuration/configuration/aboutFragment.kt
@@ -3,19 +3,19 @@ package cat.copernic.daniel.marketcomparator.ui.configuration.configuration
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.MediaController
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment.findNavController
 import androidx.navigation.fragment.findNavController
 import cat.copernic.daniel.marketcomparator.R
 import cat.copernic.daniel.marketcomparator.databinding.FragmentAboutBinding
@@ -30,7 +30,7 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 
 class aboutFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
-    private lateinit var binding:FragmentAboutBinding
+    private lateinit var binding: FragmentAboutBinding
     private val REQUEST_LOCATION_PERMISSION = 1
     private lateinit var mMap: GoogleMap
     private lateinit var myMarker: Marker
@@ -45,11 +45,10 @@ class aboutFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
             inflater,
             R.layout.fragment_about, container, false
         )
-        val mapFragment = childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
-
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-
+        startvideo()
         setHasOptionsMenu(true)
         return binding.root
     }
@@ -60,13 +59,26 @@ class aboutFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
         menu.findItem(R.id.trolley).setVisible(false)
     }
 
+    fun startvideo(){
+        val videoView: VideoView = binding.vVideo
+        val mediaController: MediaController = MediaController(requireContext())
+        mediaController.setAnchorView(videoView)
+        val offlineUri: Uri = Uri.parse("android.resource://"+ requireActivity().packageName+"/${R.raw.ejemplo}")
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(offlineUri)
+        videoView.requestFocus()
+        videoView.start()
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         val Institut = LatLng(41.56981636285994, 1.9966395571577435)
         mMap.setOnMarkerClickListener(this)
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Institut, 18F))
         myMarker = mMap.addMarker(
-            MarkerOptions().position(Institut).title("Institut Nicolau Copèrnic"))
+            MarkerOptions().position(Institut).title("Institut Nicolau Copèrnic")
+        )
 
         enableMyLocation()
 
@@ -76,8 +88,7 @@ class aboutFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
     private fun enableMyLocation() {
         if (isPermissionGranted()) {
             mMap.isMyLocationEnabled = true
-        }
-        else {
+        } else {
             ActivityCompat.requestPermissions(
                 requireActivity(),
                 arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -86,7 +97,7 @@ class aboutFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
         }
     }
 
-    private fun isPermissionGranted() : Boolean {
+    private fun isPermissionGranted(): Boolean {
         return ContextCompat.checkSelfPermission(
             requireContext(),
             Manifest.permission.ACCESS_FINE_LOCATION
@@ -106,13 +117,11 @@ class aboutFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
     }
 
     override fun onMarkerClick(marker: Marker?): Boolean {
-        if (marker!!.equals(myMarker))
-        {
+        if (marker!!.equals(myMarker)) {
             findNavController()
                 .navigate(R.id.action_aboutFragment_to_masInfoUbicacionFragment)
         }
         return true
     }
-
 
 }
